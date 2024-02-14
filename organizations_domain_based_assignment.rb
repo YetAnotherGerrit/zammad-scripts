5218f3f66e60fceaef3851d2893325f3ae59b403
Gerrit <97293098+YetAnotherGerrit@users.noreply.github.com>
2024-02-14 21:53:24 +0100

    Update organizations_domain_based_assignment.rb
    Changed where/like-string-detail.

--- organizations_domain_based_assignment.rb
+++ organizations_domain_based_assignment.rb
@@ -2,7 +2,7 @@
 
 Organization.where(domain_assignment: true).each do |org|
 	next if org.domain == nil
-	User.where(organization_id: nil).where("email LIKE '%@#{org.domain}'").each do |u|
+	User.where(organization_id: nil).where("email LIKE ?", "%@#{org.domain}").each do |u|
 		puts "Organization '#{org.name}' will be assigned to User '#{u.fullname}':"
 		u.update!(organization: org, updated_by_id: 1)
 		puts ">> User updated."
