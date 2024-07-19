ab96d16d232ec0cc7d7f145ace33e502a5ecee68
Gerrit <97293098+YetAnotherGerrit@users.noreply.github.com>
2024-07-19 10:02:19 +0200

    Update list_groups.rb
    Added "total line"

--- list_groups.rb
+++ list_groups.rb
@@ -9,3 +9,5 @@ puts "----------------------------+--------+--------+--------+--------+"
 Group.all.each do |g|
     puts "#{g.name.ljust(27)} | #{g.active.to_s.ljust(6)} | #{Ticket.where(group_id: g.id).count.to_s.rjust(6)} | #{Ticket.where(group_id: g.id, state_id: Ticket::State.find_by(name: 'closed')).count.to_s.rjust(6)} | #{Ticket.where(group_id: g.id, state_id: Ticket::State.find_by(name: 'merged')).count.to_s.rjust(6)} |"
 end
+puts "----------------------------+--------+--------+--------+--------+"
+puts "#{"Total".ljust(27)} |        | #{Ticket.all.count.to_s.rjust(6)} | #{Ticket.where(state_id: Ticket::State.find_by(name: 'closed')).count.to_s.rjust(6)} | #{Ticket.where(state_id: Ticket::State.find_by(name: 'merged')).count.to_s.rjust(6)} |"
