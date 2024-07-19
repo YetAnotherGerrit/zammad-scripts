3b47c0c89706a7c3df000c885ddf7693d5f37554
Gerrit <97293098+YetAnotherGerrit@users.noreply.github.com>
2024-07-19 11:27:02 +0200

    Update list_groups.rb
    Cosmetic upgrade

--- list_groups.rb
+++ list_groups.rb
@@ -4,10 +4,31 @@
 # This script is originally from GitHub>YetAnotherGerrit>zammad-scripts
 # Written for Zammad version 6.3.1
 
-puts "Group name                  | Active |  Count | Closed | Merged |"
-puts "----------------------------+--------+--------+--------+--------+"
-Group.all.each do |g|
-    puts "#{g.name.ljust(27)} | #{g.active.to_s.ljust(6)} | #{Ticket.where(group_id: g.id).count.to_s.rjust(6)} | #{Ticket.where(group_id: g.id, state_id: Ticket::State.find_by(name: 'closed')).count.to_s.rjust(6)} | #{Ticket.where(group_id: g.id, state_id: Ticket::State.find_by(name: 'merged')).count.to_s.rjust(6)} |"
+COLUMN_WITH = [30, 6, 6, 6, 6]
+
+def spacer_line
+    puts "+#{"".ljust(COLUMN_WITH[0]+2, "-")}+#{"".ljust(COLUMN_WITH[1]+2, "-")}+#{"".ljust(COLUMN_WITH[2]+2, "-")}+#{"".ljust(COLUMN_WITH[3]+2, "-")}+#{"".ljust(COLUMN_WITH[4]+2, "-")}+"
 end
-puts "----------------------------+--------+--------+--------+--------+"
-puts "#{"Total".ljust(27)} |        | #{Ticket.all.count.to_s.rjust(6)} | #{Ticket.where(state_id: Ticket::State.find_by(name: 'closed')).count.to_s.rjust(6)} | #{Ticket.where(state_id: Ticket::State.find_by(name: 'merged')).count.to_s.rjust(6)} |"
+
+# HEADER
+spacer_line
+puts "| #{"Group".ljust(COLUMN_WITH[0])} | #{"Active".ljust(COLUMN_WITH[1])} | #{"Count".rjust(COLUMN_WITH[2])} | #{"Closed".rjust(COLUMN_WITH[3])} | #{"Merged".rjust(COLUMN_WITH[4])} |"
+spacer_line
+
+# LIST OF GROUPS
+Group.all.each do |g|
+    print "| #{g.name.ljust(COLUMN_WITH[0])} | "
+    print "#{g.active.to_s.ljust(COLUMN_WITH[1])} | "
+    print "#{Ticket.where(group_id: g.id).count.to_s.rjust(COLUMN_WITH[2])} | "
+    print "#{Ticket.where(group_id: g.id, state_id: Ticket::State.find_by(name: 'closed')).count.to_s.rjust(COLUMN_WITH[3])} | "
+    puts  "#{Ticket.where(group_id: g.id, state_id: Ticket::State.find_by(name: 'merged')).count.to_s.rjust(COLUMN_WITH[4])} |"
+end
+
+# FOOTER
+spacer_line
+print "| #{"Total".ljust(COLUMN_WITH[0])} | "
+print "#{"".ljust(COLUMN_WITH[1])} | "
+print "#{Ticket.all.count.to_s.rjust(COLUMN_WITH[2])} | "
+print "#{Ticket.where(state_id: Ticket::State.find_by(name: 'closed')).count.to_s.rjust(COLUMN_WITH[3])} | "
+puts  "#{Ticket.where(state_id: Ticket::State.find_by(name: 'merged')).count.to_s.rjust(COLUMN_WITH[4])} |"
+spacer_line
